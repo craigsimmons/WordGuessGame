6c1ad6190ce9389be4c758f7eba6e162277fa9b3
Craig Simmons <craig.a.simmons@gmail.com>
2019-03-13 13:49:17 -0700

    skeleton web site created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,16 @@
 
+let intSum = function (intValue) {
+    return sum(range(intValue+1));
+    return intValue * (intValue+1) / 2;
+}
+intSum(8);
 
 
+
+function addInts(int) {
+    for (let i =0; i <= int; i++) {
+        let answer = answer +1;
+    }
+    return answer;
+
+}
