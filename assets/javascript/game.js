c461250ad6003c7298fcc98e3b2cb292023306d8
Craig Simmons <craig.a.simmons@gmail.com>
2019-03-18 16:44:57 -0700

    javascript not working, HTML needs polishing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -45,63 +45,61 @@ var lossCount = 0;
 var winCount = 0;
 var letterCompare;  //boolean 
 
-// Code blocks begin below:
+// Code blocks begin below
 
-//reset variables - revisit later
-guessesMade = 0;
-guessesLeft = 10;
-answerArray = [];
-missedLetters = [];
-wrongGuess = 0;
+// get buttons by their id, use variables later
+// var buttonStart = document.getElementById("start");
+// var buttonEnd = document.getElementById("end");
+// var buttonEndMusic = document.getElementById("stop");
 
-// Event handlers
-// get buttons by their id, use variables as objects later
-var buttonStart = document.getElementById("start");
-var buttonEnd = document.getElementById("end");
-var buttonEndMusic = document.getElementById("stop");
+document.getElementById("start").addEventListener("click", startGame);
+// document.getElementById("end").addEventListener("click", endGame);
+// document.getElementById("hint").addEventListener("click", getHint);
 
+function startGame()
 
-//Ends the game
-buttonEnd.addEventListener('click', function(event) {
-  document.location.href = 'http://www.microsoft.com';
-})
-
-// Starts the game
-buttonStart.addEventListener('click', function(event) {
-  // startGame()
-  // Event listener for keypress - chars saved in "keyPressed"
+   // Event listener for keypress - chars saved in "keyPressed"
   document.addEventListener('keyup', function(event) { 
-
-    // take UCS 02 values and convert to a string representing the key's value
-    keyPressed = String.fromCharCode(event.keyCode); 
-    console.log('Your letter is ' + keyPressed); // for testing
+  // take UCS 02 values and convert to a string representing the key's value
+  keyPressed = String.fromCharCode(event.keyCode); 
+  console.log('Your letter is ' + keyPressed); // for testing... REMOVE
   });
+
   // have computer pick a random word from the word array
   wordToGuess = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-    console.log("Computer's selected word " + wordToGuess);
-
+  console.log("Computer's selected word " + wordToGuess);
   // split random word into individual letters
   wordLetters = wordToGuess.split("");
     console.log("these are the letters = " + wordLetters);
 
   // create answer array and fill it with "_" and eventually guesses
   // based on length of word - using push method
-
   for (var i = 0; i < wordToGuess.length; i++) { 
       answerArray.push("_ ");
   }
-    document.getElementById("winscore").innerHTML = winCount;
-    document.getElementById("losescore").innerHTML = lossCount;
-    document.getElementById("guessesremain").innerHTML = guessesLeft;
-    document.getElementById("underscores").innerHTML = answerArray.join(" ");
-    document.getElementById("guessestaken").innerHTML = missedLetters.join(" ");
+  //code to change display fields
+  document.getElementById("currentword").innerHTML = wordToGuess;
+  document.getElementById("keytyped").innerHTML = keyPressed;
+  document.getElementById("guessesremain").innerHTML = guessesLeft;
+  document.getElementById("guessestaken").innerHTML = guessesMade;
+  document.getElementById("lettersguessed").innerHTML = missedLetters.join("  ")  
+  document.getElementById("winscore").innerHTML = winCount;
+  document.getElementById("losescore").innerHTML = lossCount;
+  document.getElementById("guessestaken").innerHTML = missedLetters.join(" ");  
+  document.getElementById("puzzle").innerHTML = answerArray.join(" ");
+    
 
-  function compareLetters (keyPressed)  {
-    letterCompare = false;
+  
+  function compareLetters (keyPressed) {
+    // letterCompare = false;
     for (var j = 0; j < wordToGuess.length; i++) {
       if (wordToGuess[i] === keyPressed)  { 
       letterCompare = true;
+      console.log("Letter Selected: " + keyPressed);
+      break;
       }
+    else {
+      console.log("key not found");
     }
   }
   if (letterCompare) {
@@ -116,13 +114,3 @@ buttonStart.addEventListener('click', function(event) {
       guessesMade++;
       guessesLeft--;
   }
-})
-
-
-
-function endRound () {
-  console.log(answerArray);
-  console.log(wordToGuess);
-
-
-}
