92e0c7364cb6ff3f4e6b59946d5de109723d0d23
Craig Simmons <craig.a.simmons@gmail.com>
2019-03-18 14:41:53 -0700

    Most of app built, need to add flow controls

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,7 +28,7 @@ var wordToGuess = "";
 // use this array to hold letters from computer's selected word
 var wordLetters = [];
 // playerSelection holds letters guessed by player
-var playerSelection = "";
+var playerSelections = "";
 //number of spaces (underlines) we display; should be equal to the selected word length
 var numSpaces = 0; //may not needthis 
 // holds info on which key was pressed
@@ -39,42 +39,90 @@ answerArray = [];
 missedLetters = [];
 // Various counters 
 // player guesses left counter starts at 10. Roughly the # of guesses if we played real hangman
-var guessCount = 0;
+var guessesMade = 0;
 var guessesLeft = 10;
 var lossCount = 0;
 var winCount = 0;
+var letterCompare;  //boolean 
 
-//Code blocks begin below:
+// Code blocks begin below:
 
-// function used to start game action
-function startGame() {
-}
-// function used to end game 
-  function endGame() {
-}
-// function to reset variables. 
-function resetAll () {
+//reset variables - revisit later
+guessesMade = 0;
+guessesLeft = 10;
+answerArray = [];
+missedLetters = [];
+wrongGuess = 0;
 
-}
+// Event handlers
+// get buttons by their id, use variables as objects later
+var buttonStart = document.getElementById("start");
+var buttonEnd = document.getElementById("end");
+var buttonEndMusic = document.getElementById("stop");
 
-// start game and computer & pick a random word from the word array
-wordToGuess = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-  console.log("Computer's selected word " + wordToGuess);
 
-// split random word into individual letters
-wordLetters = wordToGuess.split("")
-  console.log("these are the letters = " + wordLetters);
+//Ends the game
+buttonEnd.addEventListener('click', function(event) {
+  document.location.href = 'http://www.microsoft.com';
+})
 
-//create answer array and fill it with "_" and eventually guesses. 
-// based on length of word - I've coded 2 possible ways, using push method
+// Starts the game
+buttonStart.addEventListener('click', function(event) {
+  // startGame()
+  // Event listener for keypress - chars saved in "keyPressed"
+  document.addEventListener('keyup', function(event) { 
 
-/*
-for (var i = 0; i < wordToGuess.length; i++) { 
-  answerArray[i] = "";
+    // take UCS 02 values and convert to a string representing the key's value
+    keyPressed = String.fromCharCode(event.keyCode); 
+    console.log('Your letter is ' + keyPressed); // for testing
+  });
+  // have computer pick a random word from the word array
+  wordToGuess = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    console.log("Computer's selected word " + wordToGuess);
+
+  // split random word into individual letters
+  wordLetters = wordToGuess.split("");
+    console.log("these are the letters = " + wordLetters);
+
+  // create answer array and fill it with "_" and eventually guesses
+  // based on length of word - using push method
+
+  for (var i = 0; i < wordToGuess.length; i++) { 
+      answerArray.push("_ ");
+  }
+    document.getElementById("winscore").innerHTML = winCount;
+    document.getElementById("losescore").innerHTML = lossCount;
+    document.getElementById("guessesremain").innerHTML = guessesLeft;
+    document.getElementById("underscores").innerHTML = answerArray.join(" ");
+    document.getElementById("guessestaken").innerHTML = missedLetters.join(" ");
+
+  function compareLetters (keyPressed)  {
+    letterCompare = false;
+    for (var j = 0; j < wordToGuess.length; i++) {
+      if (wordToGuess[i] === keyPressed)  { 
+      letterCompare = true;
+      }
+    }
+  }
+  if (letterCompare) {
+    for (var k = 0; k < wordToGuess.length; j++) {
+      if (wordToGuess[j] === keyPressed) {
+        answerArray[j] = keyPressed;
+      }
+    }
+  }
+  else {
+    missedLetters.push(keyPressed);
+      guessesMade++;
+      guessesLeft--;
+  }
+})
+
+
+
+function endRound () {
   console.log(answerArray);
-}
-*/
-for (var i = 0; i < wordToGuess.length; i++) { 
-    answerArray.push("")
-    console.log(answerArray);
+  console.log(wordToGuess);
+
+
 }
