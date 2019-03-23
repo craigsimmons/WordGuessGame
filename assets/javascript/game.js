dfb4fa574b9ae985a8300bddd94f921681a257da
Craig Simmons <craig.a.simmons@gmail.com>
2019-03-23 12:20:33 -0700

    updated start/stop/continue logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// Global variable and array declarations follow below:
+/* Global variable and array declarations follow below: */
 var wordsListArr = ["pneumonia",  // Seed wordArray with values
   "anthrax",
   "diphtheria",
@@ -28,37 +28,42 @@ var missedLettersArr = []; // Array to hold incorrectly guessed letters
 var guessesLeftInt= 10; // player guesses left counter starts at 10.
 var lossCountInt = 0; // loss counter
 var winCountInt = 0; // win counter
-var letterCompareBoo = true;  // boolean 
+var letterCompareBoo = false;  // boolean 
 
 /* ------------- End variable declarations  ------------------- */
 
 /* Event handlers */
 $("#start").on("click", startGame); // select button and give an onclick function -- jQuery
-
 $("#end").on("click", startGame); // select button and give an onclick function -- jQuery
 
-$(document).keyup(function(event) { // Capture keyup event w/ jQuery
-  keyPresseStr = String.fromCharCode(event.keyCode).toLowerCase(); //translate keycode to charcode and make lowercase
-  console.log('Your letter is ' + keyPressedStr); // for testing... 
-});
 
-function selectWord () { 
-  wordToGuessStr = wordsListArr[Math.floor(Math.random() * wordsListArr.length)];
-    console.log("Computer's selected word " + wordToGuessStr);
-  wordLettersArr = wordToGuessStr.split(""); // may not need
-    console.log("these are the letters = " + wordLettersArr);
-}
+
 
 function startGame () {
-
+  selectWord();
+  resetVariables();
 }
 
 function endGame () {
-   
+  resetVariables();
+  // Thanks for playiong come back
+}
+
+function continueGame {
+  selectWord();
 }
 
 function resetVariables () {
-
+  wordToGuessStr = "";
+  wordLettersArr = [];
+  keyPressedStr = ""; 
+  answerArrayArr = []; 
+  remainingLettersInt = 0;
+  missedLettersArr = [];
+  guessesLeftInt= 10;
+  lossCountInt = 0; 
+  winCountInt = 0; 
+  letterCompareBoo = false
 }
 
 function displayValues () {
@@ -71,6 +76,51 @@ function displayValues () {
   $("#puzzle").text(answerArrayArr.join(" "));
 }
 
+function winGame () {
+  $("#lettersguessed").text(missedLettersArr.join("  "));
+  $("#puzzle").text(answerArrayArr.join(" "));
+  console.log("You guessed the word - " + ToGuessStr + " in": + guessesLeftInt + " turns");
+  console.log("Here's the answer array: " + answerArrayArr);
+  console.log("Here are the incorrect guesses: " + missedLettersArr);
+  console.log("Winner winner chicken dinner"); // repace with real win status msg
+  winCountInt = winCountInt +1;
+  $("#winscore").text(winCountStr);
+  console.log("# of wins: " + winCountInt);
+}
+
+function loseGame () {
+  console.log("Soy un perdedor"); // repace with real loss status msg
+  console.log("Here's the answer array for loss: " + answerArrayArr);
+  console.log("Here are the incorrect guesses: " + missedLettersArr);
+  lossCountInt = lossCountInt +1;
+  $("#losescore").text(lossCountStr);
+  console.log("# of losses: " + lossCountInt);
+}
+
+function selectWord () { 
+  wordToGuessStr = wordsListArr[Math.floor(Math.random() * wordsListArr.length)];
+    console.log("Computer's selected word " + wordToGuessStr);
+  wordLettersArr = wordToGuessStr.split(""); // may not need
+    console.log("these are the letters = " + wordLettersArr);
+    prepAnswerArray();
+}
+
+function prepAnswerArray () {
+  for ( var i = 0; i < wordToGuessStr.length; i++) {
+    answerArrayArr[i] = "_";
+  }
+  $("#puzzle").text(answerArrayArr.join(" ")); //display ____ on screen
+  inputLetter();
+}
+
+function inputLetter () {
+  $(document).keyup(function(e) { // Capture keyup event w/ jQuery
+    keyPresseStr = String.fromCharCode(event.keyCode).toLowerCase(); //translate keycode to charcode and make lowercase
+    console.log('Your letter is ' + keyPressedStr); // for testing... 
+  });
+    validateInput();
+  }
+
 function validateInput () {
   if (keyPressedStr === null) {
     alert("Please select a single letter");
@@ -80,17 +130,16 @@ function validateInput () {
     alert("Please select a single letter");
     validateInput();
   }
-  else { 
+  else if (missedLettersArr.indexOf(keyPressedStr) === -1) {
+    alert("You already selected this letter. Choose again.");
+    validateInput();
+  }
+  else {
     remainingLettersInt = wordToGuessStr.length;
+    $("#guessesremain").text(guessesLeftStr);
+    $("#lettersguessed").text(missedLettersArr.join("  "));
     evaluateGuess(); 
   }
-}
-
-function populateAnswerArray () {
- for ( var i = 0; i < wordToGuessStr.length; i++) {
-   answerArrayArr[i] = "_";
- }
- $("#puzzle").text(answerArrayArr.join(" "));
 }
 
 function evaluateGuess() {
@@ -99,23 +148,30 @@ function evaluateGuess() {
       if (wordToGuessStr[j] === keyPressedStr)  { 
         answerArrayArr[j] = keyPressedStr;
         remainingLettersInt--;
-        letterCompareBoo = true;
+
+        console.log("running count of guesses: " + answerArrayArr);
+        console.log("# letters evaluated" + remainingLettersInt);
       }
       else {
         missedLettersArr.push(keyPressedStr);
+        console.log("Here are the incorrect guesses: " + missedLettersArr);
       }
     }
   }
-}
 
-function checkGameState () {
-  if (answerArrayArr.indexOf("_") === -1) { // entire word is processed; no __
-    //check for game state change
+  // make this gameState function
+  if (answerArrayArr.indexOf("_") === -1) { // checks array to see if there are any "_" if none, word was guessed
+    winGame();
+  } 
+  else if (answerArrayArr.indexOf("_") !== -1) {
+    // could also use remainingLettersInt = 0)  
+    loseGame();
   }
-
-
+  else {
+    continueGame();
+  }
 }
-
+  
 /*
 0. Enable Keyup and on click events
 1. Pick Random Word
